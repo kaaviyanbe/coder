9b752d10723eaedcfe5f36ba80a866037351e7da
kaaviyanbe <kaaviyanbe@gmail.com>
2016-08-16 18:07:10 +0530

    Create arr.java

---
+++ arr.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class arr
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+StringBuffer str=new StringBuffer(sc.next());
+str.reverse();
+System.out.println(str);
+}
+}
